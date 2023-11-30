7a7e6a569f56cfd478bda4d9050e6951c4e17fa9
Alex <alex@alexcuan.com>
2023-11-30 15:36:14 +0100

    Add option to move left

--- src/Main.java
+++ src/Main.java
@@ -477,6 +477,8 @@ public class Main {
                 movePieceDown();
             } else if (input.equalsIgnoreCase("d")) {
                 movepiecerightonbigboard();
+            } else if (input.equalsIgnoreCase("a")) {
+                movePieceLeftOnBigBoard();
             }
         }
     }
